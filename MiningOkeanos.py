874dae2b8958132410a9805a25ff2ded3c5573a8
Aiden Golub <23golaid@hawken.edu>
2021-04-05 19:39:08 -0400

    Update MiningOkeanos.py

--- MiningOkeanos.py
+++ MiningOkeanos.py
@@ -4,11 +4,20 @@ def ConvertCuToAg():
 		if "Miner's Pick" in Inventory:
 			Inventory.remove("Copper")
 			Inventory.append("Silver")
+			print("Your Copper has successfuly been converted into Silver!")
+		else:
+			print("You don't have the Miner's Pick!")
+	else:
+		print("You don't have any Copper!")
 def ConvertAgToAu():
 	if "Silver" in Inventory:
 		if "Miner's Pick" in Inventory:
 			Inventory.remove("Silver")
 			Inventory.append("Gold")
+			print("Your Silver has successfuly been converted into Gold!")
+		else:
+			print("You don't have the Miner's Pick!")
+	else:
+		print("You don't have any Silver!")			
 #ConvertCuToAg()
 #ConvertAgToAu()
-
