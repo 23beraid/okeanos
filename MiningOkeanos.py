b4ccccc7c37d0b614e1feeaf8f0b1b87070bec32
Aiden Golub <23golaid@hawken.edu>
2021-04-05 19:29:35 -0400

    Update MiningOkeanos.py

--- MiningOkeanos.py
+++ MiningOkeanos.py
@@ -1,32 +1,14 @@
-import time
-def Copper():
-	Copper.CopperAmount = 1
-def Silver():
-	Silver.SilverAmount = 1
-def Gold():
-	Gold.GoldAmount = 1
-Inventory = ["Miner's Pick"]
-#you can always add on to the inventory later on
-	def ConvertCuToAg():
-		Copper()
-		if Copper.CopperAmount > 0:
-			if "Miner's Pick" in Inventory:
-				Copper()
-				Silver()
-				Copper.CopperAmount -= 1
-				time.sleep(7200)
-				Silver.SilverAmount+=1
-	def ConvertAgToAu():
-		Silver()
-		if Silver.SilverAmount > 0:
-			if "Miner's Pick" in Inventory:
-				Silver()
-				Gold()
-				Silver.SilverAmount-=1
-				time.sleep(7200)
-				Gold.GoldAmount+=1
+Inventory = ["Miner's Pick", "Copper", "Silver", "Gold"]
+def ConvertCuToAg():
+	if "Copper" in Inventory:
+		if "Miner's Pick" in Inventory:
+			Inventory.remove("Copper")
+			Inventory.append("Silver")
+def ConvertAgToAu():
+	if "Silver" in Inventory:
+		if "Miner's Pick" in Inventory:
+			Inventory.remove("Silver")
+			Inventory.append("Gold")
 #ConvertCuToAg()
 #ConvertAgToAu()
 
-#If the player has a (Miner's Pick) in their invertory, they unlock the two functions above
-#The player can now state either function to convert.
