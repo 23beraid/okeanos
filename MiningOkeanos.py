0df7f363ff194fd74d1b8e0636722eccbe51a916
Aiden Golub <23golaid@hawken.edu>
2021-04-05 19:12:48 -0400

    Update MiningOkeanos.py

--- MiningOkeanos.py
+++ MiningOkeanos.py
@@ -7,19 +7,24 @@ def Gold():
 	Gold.GoldAmount = 1
 Inventory = ["Miner's Pick"]
 #you can always add on to the inventory later on
-if "Miner's Pick" in Inventory:
 	def ConvertCuToAg():
 		Copper()
-		Silver()
-		Copper.CopperAmount -= 1
-		time.sleep(7200)
-		Silver.SilverAmount+=1
+		if Copper.CopperAmount > 0:
+			if "Miner's Pick" in Inventory:
+				Copper()
+				Silver()
+				Copper.CopperAmount -= 1
+				time.sleep(7200)
+				Silver.SilverAmount+=1
 	def ConvertAgToAu():
 		Silver()
-		Gold()
-		Silver.SilverAmount-=1
-		time.sleep(7200)
-		Gold.GoldAmount+=1
+		if Silver.SilverAmount > 0:
+			if "Miner's Pick" in Inventory:
+				Silver()
+				Gold()
+				Silver.SilverAmount-=1
+				time.sleep(7200)
+				Gold.GoldAmount+=1
 #ConvertCuToAg()
 #ConvertAgToAu()
 
