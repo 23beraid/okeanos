2c593514a1ba188b35867022e120e5fcd19cf1ca
Aiden Golub <23golaid@hawken.edu>
2021-04-06 10:22:44 -0400

    Update ArtifactandReward.py

--- ArtifactandReward.py
+++ ArtifactandReward.py
@@ -7,30 +7,12 @@ def adventureReward():
 	Mineral_list = ["Ruby","Emerald","Aquamarine","Topaz","Diamond","Amythyst"]
 	RanList = [1,2,3,4,5,6,7,8,9,10]
 	randomran = random.choice(RanList)
-	if randomran == 1:
-		return(random.choice(Artifact_list))
-	elif randomran == 2:
-		return(random.choice(Artifact_list))
-	elif randomran == 3:
-		return(random.choice(Artifact_list))
-	elif randomran == 4:
-		return(random.choice(Artifact_list))
-	elif randomran == 5:
-		return(random.choice(Artifact_list))
-	elif randomran == 6:
-		return(random.choice(Artifact_list))
-	elif randomran == 7:
-		return(random.choice(Artifact_list))
-	elif randomran == 8:
-		return(random.choice(Artifact_list))
-	elif randomran == 9:
-		return(random.choice(Mineral_list))
-	elif randomran == 10:
-		return(random.choice(Mineral_list))
+	Mineral_list = random.choice(Mineral_list)
+	Artifact_list = random.choice(Artifact_list)
 	if ramdomran == 9:
-		return "You got", Mineral_List+"!"
+		return Mineral_List
 	elif ramdomran == 10:
-		return "You got", Mineral_List+"!"
+		return Mineral_List
 	else:
-		return "You got", Artifact_List+"!"
+		return Artifact_List
 
