d28b7c364bfa5a467e3d1518accc18e38adb8e3d
Aiden Golub <23golaid@hawken.edu>
2021-04-05 20:07:49 -0400

    Update BankOkeanos.py

--- BankOkeanos.py
+++ BankOkeanos.py
@@ -2,20 +2,16 @@ Inventory = ["Miner's Pick", "Copper", "Silver", "Gold", "Copper"]
 def Convert1AgTo2Cu():
 	if "Silver" in Inventory:
 		Inventory.remove("Silver")
-		x=0
 		for x in range (0,2):
 			Inventory.append("Copper")
-			x+=1
 		return "Your Silver has successfuly been converted into 2 Copper!"
 	else:
 		return "You don't have any Silver!"
 def Convert1AuTo3Cu():
 	if "Gold" in Inventory:
 		Inventory.remove("Gold")
-		x=0
 		for x in range (0,3):
 			Inventory.append("Copper")
-			x+=1
 		return "Your Gold has successfuly been converted into 3 Copper!"
 	else:
 		return "You don't have any Gold!"	
