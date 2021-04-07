08733475fcf3b7aee1ec82f3ef4befd257dc94c3
sanjay-sneha <81997792+sanjay-sneha@users.noreply.github.com>
2021-04-07 10:30:52 -0400

    Update market_draft.py

--- market_draft.py
+++ market_draft.py
@@ -20,8 +20,11 @@ def buy_item():
     print("Work in progress")
     #create purchase function
     
-def sell_item():
-    i = input("Which item would you like to sell? Type here: ")
+def in_sell_item():
+    #i = input("Which item would you like to sell? Type here: ")
+    sell_item(i)
+    
+def sell_item(i):
     if (" " in i):
         first = True
         i2 = i.split()
@@ -36,13 +39,15 @@ def sell_item():
         i = i.capitalize()
     if i not in artifacts and i not in minerals and i not in tools:
         print("Not a valid artifact. Try again.")
-        sell_item()
+        in_sell_item()
     else:
-        enter_price(i)
+        enter_price(i,j)
     #exchange currency
     
-def enter_price(i):
-    j = input("Enter the price you would like to put it on the market as, in units of copper: ")
+def in_enter_price():
+    #j = input("Enter the price you would like to put it on the market as, in units of copper: ")
+    
+def enter_price(i,j):
     if j.isdigit():
         if i in mPrices:
             mPrices[i].append(int(j))
@@ -51,7 +56,7 @@ def enter_price(i):
         print("Your item, " + i + ", has been put on the market at the price of " + j + " copper! It is now waiting to be ordered.")
     else:
         print("Not a valid price. Try again.")
-        enter_price(i)
+        enter_price(i,j)
     
 def display_items():
     txt = ""
