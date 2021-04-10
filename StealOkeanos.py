bea855479dbdda1eafc771861fb61732478b7128
Aiden Golub <23golaid@hawken.edu>
2021-04-10 13:56:38 -0400

    Add files via upload

---
+++ StealOkeanos.py
@@ -0,0 +1,16 @@
+#Inventory=["Ruby", "Emerald", "Aquamarine", "Mages Robe"]
+#Victiminv=["Ruby", "Emerald", "Aquamarine", "Mages Robe"]
+Victiminv=db['inv'str(userid)]
+Inventory=db['inv'+str(message.author.id)]
+def StealfromVictim(Victiminv,Inventory,userid):
+	if "Mages Robe" in Inventory:
+		i=input("What item do you want to steal: ")
+		for x in Victiminv:
+			if x == i:
+				Inventory.append(x)
+				Victiminv.remove(x)
+				break
+	else:
+		return "You need a Mages Robe to steal"
+#StealfromVictim(Victiminv,Inventory,userid)
+
