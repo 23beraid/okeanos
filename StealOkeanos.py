0a13a1c88e4db03d42ea959ab090bb0ca0bbe128
Aiden Golub <23golaid@hawken.edu>
2021-04-10 19:46:19 -0400

    Update StealOkeanos.py

--- StealOkeanos.py
+++ StealOkeanos.py
@@ -1,14 +1,17 @@
 Victiminv=db['inv'str(userid)]
 Inventory=db['inv'+str(message.author.id)]
 def StealfromVictim(Victiminv,Inventory,userid):
-	if "Mages Robe" in Inventory:
-		i=input("What item do you want to steal: ")
-		for x in Victiminv:
-			if x == i:
-				Inventory.append(x)
-				Victiminv.remove(x)
-				break
+	if "Mages robe" not in Victiminv:
+		if "Warriors helm" in Inventory:
+			i=input("What item do you want to steal: ")
+			for x in Victiminv:
+				if x == i:
+					Inventory.append(x)
+					Victiminv.remove(x)
+					break
+		else:
+			return "You need a Mages Robe to steal"
 	else:
-		return "You need a Mages Robe to steal"
+		return "The recipient can not be stolen from"
 #StealfromVictim(Victiminv,Inventory,userid)
 
