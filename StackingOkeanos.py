e5afb66c75c6f5d7b2f784f08a92f9e07749abe4
Aiden Golub <23golaid@hawken.edu>
2021-04-11 10:59:05 -0400

    Add files via upload

---
+++ StackingOkeanos.py
@@ -0,0 +1,18 @@
+#This will seperate the unique items, count them, and convert into dictionary. SI_dct is the new Inventory dictionary.
+Inventory = ["Miner's Pick", "Copper", "Silver", "Gold", "Copper", "Gold", "Gold"]
+UI=[]
+SI=[]
+def UniqueGen(UI, Inventory):
+	for x in Inventory:
+		if x not in UI:
+			UI.append(x)
+def Convert2Dct(SI):
+    SI_dct = {SI[i]: SI[i + 1] for i in range(0, len(SI), 2)}
+def StackInventory(UniqueGen,UI,Inventory):
+	UniqueGen(UI, Inventory)
+	for x in UI:
+		SI.append(x)
+		SI.append(str(Inventory.count(x)))
+	Convert2Dct(SI)
+
+StackInventory(UniqueGen,UI,Inventory)
