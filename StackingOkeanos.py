66140fd7b08362e866dec5a8364f2f580e06332e
23beraid <64813542+23beraid@users.noreply.github.com>
2021-04-21 20:16:03 -0400

    Update StackingOkeanos.py
    >Fixed indentation
    >Moved variable declarations inside function
    >fixed invocation of variable SI_dct

--- StackingOkeanos.py
+++ StackingOkeanos.py
@@ -1,15 +1,17 @@
 #This will seperate the unique items, count them, and convert into dictionary. SI_dct is the new Inventory dictionary.
 Inventory = ["Miner's Pick", "Copper", "Silver", "Gold", "Copper", "Gold", "Gold"]
-UI=[]
-SI=[]
+#UI=[]
+#SI=[]
 def UniqueGen(UI, Inventory):
 	for x in Inventory:
 		if x not in UI:
 			UI.append(x)
 def Convert2Dct(SI):
-    SI_dct = {SI[i]: SI[i + 1] for i in range(0, len(SI), 2)}
-	return "SI_dct"
-def StackInventory(UniqueGen,UI,Inventory):
+    	SI_dct = {SI[i]: SI[i + 1] for i in range(0, len(SI), 2)}
+	return SI_dct
+def StackInventory(UniqueGen,Inventory):
+	UI=[]
+	SI=[]
 	UniqueGen(UI, Inventory)
 	for x in UI:
 		SI.append(x)
